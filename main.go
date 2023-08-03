ba8aeca6919fdef8d242d247c2b8033f8ea00d9f
majingzhen <matuto@qq.com>
2023-08-03 16:31:40 +0800

    初始化

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "go-gen2pdm/gen"
 
 func main() {
-	gen.Pdm2xml("E:\\project\\xwt\\TSQC\\03概要设计\\04-数据库设计\\tsqcV1.0.5.pdm", false)
+	gen.Pdm2xml("./pdm/测试数据库模型 - 副本.PDM", true)
 }
